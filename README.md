7e4d27297f64fa4b6db60cafeb987ff2b8bec1b5
Juliana dos Santos Freitas <jujugemeasf@gmail.com>
2021-05-11 13:20:53 -0300

    full game

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Memory-Game
  Criação do jogo da memória.
+
+## Tecnologias utilizadas
+- HTML5;
+- CSS3;
+- Javascript.
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function fliipCard() {
+  if(lockBoard) return; // nada vai acontecer caso seja true
+  if(this === firstCard) return; // nada vai acontecer caso clicar na mesma carta
+  
   this.classList.add('flip');
   if(!hasFlippedCard){
     hasFlippedCard = true;
@@ -15,14 +19,51 @@ function fliipCard() {
   checkforMath();
 }
 
+// checando as cartas
 function checkforMath(){
-  if(firstCard.dataset.card === secondCard.dataset.card){
+  if(firstCard.dataset.card === secondCard.dataset.card){ //se as cartas forem iguais
     disableCards();
     return;
   }
 
   unflipCards();
 }
+
+// Desabilitar o click
+function disableCards(){
+  firstCard.removeEventListener('click', fliipCard);
+  secondCard.removeEventListener('click', firstCard)
+
+  resertBoard();
+}
+
+//fazer com que as cartas virem caso não forem iguais
+function unflipCards(){
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    
+    resertBoard();
+  }, 1500)
+}
+
+// setar a variavel 
+function resertBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+//função que é renderizada sempre que chamada
+(//embaralhar as cartas
+function shuffle(){
+  cards.forEach((card) => {
+    //sorteio das 12 cartas
+    let radomPosition = Math.floor(Math.random() * 12);
+    //.radom sortear aos numeros e o .floor arredondar para inteiro
+    card.style.order = radomPosition;
+  })
+})();
 
 cards.forEach((card) => {
   card.addEventListener('click', fliipCard)
